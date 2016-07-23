18b86a398725c4d87c4436ad158f6847412482ec
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-07-24 00:02:48 +0300

    add country and Currency classes

---
+++ src/main/java/hello/Currency.java
@@ -0,0 +1,38 @@
+package hello;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for currency.
+ * <p>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * <simpleType name="currency">
+ *   <restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     <enumeration value="GBP"/>
+ *     <enumeration value="EUR"/>
+ *     <enumeration value="PLN"/>
+ *   </restriction>
+ * </simpleType>
+ * </pre>
+ */
+@XmlType(name = "currency")
+@XmlEnum
+public enum Currency {
+
+    GBP,
+    EUR,
+    PLN;
+
+    public String value() {
+        return name();
+    }
+
+    public static Currency fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
